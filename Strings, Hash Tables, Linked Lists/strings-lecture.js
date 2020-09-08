ae24d1d00bf74c1c4aa1c89efcaaab7076c9e803
Christian-Tirado-CODE <tiradoc02@gmail.com>
2020-09-08 10:17:56 -0400

    Lecture on Strings

---
+++ Strings, Hash Tables, Linked Lists/strings-lecture.js
@@ -0,0 +1,26 @@
+/*
+
+Strings, What are they?
+In programming, strings are data types that store sequences of characters. 
+Depending on the language you’re using, strings may or may not be immutable. 
+In Java specifically, strings are immutable. This is to help ensure things like
+security, synchronization, and caching. Because strings are immutable, when you 
+modify a string, it creates an entirely new string. This is important to know, 
+especially during technical interviews, as repeatedly modifying a string could 
+degrade your runtime. If you do need to constantly modify a string during an 
+interview question, consider using a StringBuilder, which is mutable.
+
+
+String Methods to Know for Interviews
+Strings are arguably the most commonly used data type throughout programming, 
+and as a result, will be used extensively during interviews. Whether the problem 
+relates to strings directly and manipulating them, or you simply need to store 
+intermediary information to solve the problem at hand, you should familiarize 
+yourself with strings and their available methods. Some of the most common 
+string methods you’ll use during interviews are the following:
+
+.toCharArray()
+.indexOf()
+.split()
+
+ */
