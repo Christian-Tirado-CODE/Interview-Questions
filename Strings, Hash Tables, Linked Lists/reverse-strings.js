dd0bfde4d03bfd4b33ff10b8f3e75086f45c88a4
Christian-Tirado-CODE <tiradoc02@gmail.com>
2020-09-08 10:12:54 -0400

    reverse-strings
    Added my first interview question practice.

---
+++ Strings, Hash Tables, Linked Lists/reverse-strings.js
@@ -0,0 +1,15 @@
+function reverseString(str){
+    const arr = str.split('');
+    let reversedStr = '';
+    
+    for(let i=arr.length-1; i >= 0; i--){
+        reversedStr += arr[i];
+    }
+  
+  return reversedStr;
+}
+//Complexity: O(N) where n is the size of the string.
+
+console.log(reverseString('cat'));
+console.log(reverseString('The Daily Byte'));
+console.log(reverseString('civic'));
