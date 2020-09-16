be993e0909d05e2a6bf5a2bc7682f85013c6d3cc
Christian-Tirado-CODE <tiradoc02@gmail.com>
2020-09-16 08:14:34 -0400

    Update palindrome-with-removal.js

--- Strings, Hash Tables, Linked Lists/palindrome-with-removal.js
+++ Strings, Hash Tables, Linked Lists/palindrome-with-removal.js
@@ -1,4 +1,4 @@
-function isPalindrome(str){
+/*function isPalindrome(str){
     let isCharDeleted = false;
     const middle = Math.ceil(str.length / 2) + 1;
   
@@ -18,9 +18,39 @@ function isPalindrome(str){
     }
   console.log(str);
   return true;  
-}
+}*/
 // Time Complexity: O(N)
 // Space Complexity: O(N) In the worst case The string has to copied
+
+function validPalindrome(s) {
+  let i = 0, j = s.length - 1;
+  while(i < j) {
+      if(s.charAt(i) != s.charAt(j)) {
+          return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+      }
+
+      i++;
+      j--;
+  }
+
+  return true;
+}
+
+function isPalindrome(s, i, j) {
+  while(i < j) {
+      if(s.charAt(i++) != s.charAt(j--)) {
+          return false;
+      }
+  }
+
+  return true;
+}
+//Source: The Daily Byte
+// Time Complexity: O(N)
+
+console.log(validPalindrome("abcba"));
+console.log(validPalindrome("foobof"));
+console.log(validPalindrome("abccab"));
 
 console.log(isPalindrome("abcba"));
 console.log(isPalindrome("foobof"));
